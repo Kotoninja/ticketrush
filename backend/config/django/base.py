54a20830ebc7e0f0609ab943b79990ad71d1deb4
artem <SS4T46@yandex.ru>
2026-06-30 23:46:50 +0300

    feat: add drf

--- backend/config/django/base.py
+++ backend/config/django/base.py
@@ -28,12 +28,17 @@ ALLOWED_HOSTS = env("DJANGO_ALLOWED_HOSTS")
 # Application definition
 
 INSTALLED_APPS = [
+    # Django apps
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # My apps
+    # Third party-packages
+    "rest_framework"
+
 ]
 
 MIDDLEWARE = [
@@ -117,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+from config.settings.rest_framework import *
