fb8afc327b60e9460b2243b14158efbb4d12c812
artem <SS4T46@yandex.ru>
2026-07-01 21:17:31 +0300

    feat: update reference_id in organization model

--- backend/organization/models.py
+++ backend/organization/models.py
@@ -1,16 +1,23 @@
-import uuid
-
+import secrets
 from common.models import BaseModel
 from django.db import models
 from user.models import CustomUser
 
 
+def generate_reference_id() -> str:
+    return secrets.token_hex()[:8]
+
+
 class Organization(BaseModel):
     created_by = models.ForeignKey(to=CustomUser, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=255)
-    reference_id = models.UUIDField(
-        default=uuid.uuid4, editable=False, unique=True, null=True
+    reference_id = models.CharField(
+        default=generate_reference_id(),
+        editable=False,
+        unique=True,
+        null=True,
+        max_length=8,
     )
 
     def __str__(self) -> str:
-        return f"{self.name} / ref_id:{str(self.reference_id)[:10]}"
+        return f"{self.name} / ref_id:{str(self.reference_id)[:8]}"
