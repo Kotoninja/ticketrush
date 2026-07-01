c081274fddacc7c3d7244bb8aa715549a39d4e63
artem <SS4T46@yandex.ru>
2026-07-01 21:09:24 +0300

    feat: update common model

--- backend/common/models.py
+++ backend/common/models.py
@@ -1,10 +1,10 @@
-from django.db import models
-from django.utils import timezone
 from typing import Final
+
+from django.db import models
 
 
 class BaseModel(models.Model):
-    created_at: Final = models.DateTimeField(db_index=True, default=timezone.now)
+    created_at: Final = models.DateTimeField(db_index=True, auto_now_add=True)
     updated_at: Final = models.DateTimeField(auto_now=True)
 
     class Meta:
