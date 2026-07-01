023f08410a263df32fc7b0366e9d42273c25eca8
artem <SS4T46@yandex.ru>
2026-07-01 21:07:22 +0300

    feat: add organization model

--- backend/organization/admin.py
+++ backend/organization/admin.py
@@ -1,5 +1,5 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# from .models import Organization
+from .models import Organization
 
-# admin.site.register(Organization)
+admin.site.register(Organization)
--- backend/organization/models.py
+++ backend/organization/models.py
@@ -1,8 +1,16 @@
-# from common.models import BaseModel
-# from django.db import models
-# from user.models import CustomUser
+import uuid
+
+from common.models import BaseModel
+from django.db import models
+from user.models import CustomUser
 
 
-# class Organization(BaseModel):
-#     created_by = models.ForeignKey(to=CustomUser, on_delete=models.SET_NULL, null=True)
-#     name = models.CharField(max_length=255)
+class Organization(BaseModel):
+    created_by = models.ForeignKey(to=CustomUser, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=255)
+    reference_id = models.UUIDField(
+        default=uuid.uuid4, editable=False, unique=True, null=True
+    )
+
+    def __str__(self) -> str:
+        return f"{self.name} / ref_id:{str(self.reference_id)[:10]}"
