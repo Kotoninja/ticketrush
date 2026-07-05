43db66e39e43b0bc23c99f07883fdc1d26f821f8
artem <SS4T46@yandex.ru>
2026-07-05 20:51:50 +0300

    feat: update venue serializer

--- backend/venue/serializers.py
+++ backend/venue/serializers.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
 
 from .models import Venue
@@ -7,3 +8,13 @@ class VenueSerializer(serializers.ModelSerializer):
     class Meta:
         model = Venue
         fields = "__all__"
+
+    def validate(self, attrs):
+        instance = self.Meta.model(**attrs)
+
+        try:
+            instance.full_clean()
+        except ValidationError as e:
+            raise serializers.ValidationError(e)
+
+        return super().validate(attrs)
