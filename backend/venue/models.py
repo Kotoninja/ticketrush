3d0822823ec0a3146de474d8657235f5b3b2f662
artem <SS4T46@yandex.ru>
2026-07-05 20:36:02 +0300

    feat: update venue model

--- backend/venue/models.py
+++ backend/venue/models.py
@@ -1,29 +1,39 @@
 from common.models import BaseModel
 from django.db import models
 from organization.models import Organization
+from django.db import transaction
 
 from .validators import name_validation_for_a_exists_organization
 
 
 class Venue(BaseModel):
     created_by = models.ForeignKey(
-        to=Organization, on_delete=models.PROTECT, null=True, related_name="venues"
+        to=Organization,
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        related_name="venues",
     )
     description = models.CharField(max_length=400)
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=100)
-    site_link = models.URLField(null=True)
+    site_link = models.URLField(null=True, blank=True)
 
     class Meta:
         constraints = [
             models.UniqueConstraint(
                 fields=["name", "address"], name="unique_venue_name_per_address"
-            )
+            ),
         ]
 
     def clean(self):
-        super().clean()
         name_validation_for_a_exists_organization(self)
+        super().clean()
+
+    def save(self, *args, **kwargs):
+        with transaction.atomic():
+            self.full_clean()
+            super().save(*args, **kwargs)
 
     def __str__(self) -> str:
         org_name = self.created_by.name if self.created_by else "No Organization"
