a9a8485df513302037c4ce7c11c56518a6115fcf
artem <SS4T46@yandex.ru>
2026-07-09 21:32:10 +0300

    feat: add query parameter to a list action
    add hall query_parametr to a EventSessionAPI for filter events by hall

--- backend/session/views.py
+++ backend/session/views.py
@@ -1,10 +1,12 @@
 from django.db import transaction
 from django.shortcuts import get_object_or_404
 from rest_framework import status, viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from .models import EventSession
 from .serializers import EventSessionReadSerializer, EventSessionWriteSerializer
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 
 
 class EventSessionAPI(viewsets.ModelViewSet):
@@ -21,8 +23,19 @@ class EventSessionAPI(viewsets.ModelViewSet):
     def perform_create(self, serializer: EventSessionWriteSerializer):
         self.instance = serializer.save()
 
+    @extend_schema(
+        parameters=[
+            OpenApiParameter("hall", type=int, description="Sort events by venue")
+        ]
+    )
     def list(self, request):
-        serializer = self.get_serializer(self.get_queryset(), many=True)
+
+        events = self.get_queryset()
+
+        if get_hall := request.query_params.get("hall", None):
+            events = events.filter(hall=get_hall)
+
+        serializer = self.get_serializer(events, many=True)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def create(self, request):
