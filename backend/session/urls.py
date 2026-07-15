2234b53e55d05ffa6153a5250b90f7eb2d573886
artem <SS4T46@yandex.ru>
2026-07-15 14:35:37 +0300

    feat: add SeatSession API

--- backend/session/urls.py
+++ backend/session/urls.py
@@ -1,8 +1,16 @@
-from rest_framework import routers
 from django.urls import path
-from .views import EventSessionAPI, index
+from rest_framework import routers
+
+from .views import EventSessionAPI, SeatSessionAPI, index
 
 router = routers.SimpleRouter()
 router.register("", EventSessionAPI)
 
-urlpatterns = [path("index/", index  )] + router.urls
+urlpatterns = [
+    path("index/", index),
+    path(
+        "seat/<int:event_session_pk>/<int:seat_session_pk>",
+        SeatSessionAPI.as_view(),
+        name="seat-session-detail",
+    ),
+] + router.urls
