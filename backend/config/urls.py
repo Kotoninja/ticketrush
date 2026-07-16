f0699e607b5eb86e2f9bc8926a558054a2864225
artem <SS4T46@yandex.ru>
2026-07-16 17:02:40 +0300

    feat: add django-debug-toolbar

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,15 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
-from drf_spectacular.views import SpectacularSwaggerView
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
     SpectacularSwaggerView,
 )
-
 
 urlpatterns = [
     # Django
@@ -44,3 +44,12 @@ urlpatterns = [
     path("api/session/", include("session.urls")),
     path("api/booking/", include("booking.urls")),
 ]
+
+
+# Django Debug Toolbar
+if not settings.TESTING:
+    from debug_toolbar.toolbar import debug_toolbar_urls
+
+    urlpatterns = [
+        *urlpatterns,
+    ] + debug_toolbar_urls()
