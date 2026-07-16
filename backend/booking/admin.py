47d03d7031ff13659a1db9ed6b15a6ae95c24b4c
artem <SS4T46@yandex.ru>
2026-07-17 01:00:02 +0300

    feat: update booking admin

--- backend/booking/admin.py
+++ backend/booking/admin.py
@@ -1,5 +1,24 @@
+from typing import Any
+
 from django.contrib import admin
+from django.forms.models import ModelForm
+from django.http import HttpRequest
+
 from .models import Booking
+from .services import BookingService
 
 
-admin.site.register(Booking)
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    def save_model(
+        self, request: HttpRequest, obj: Any, form: ModelForm, change: bool
+    ) -> None:
+        if change:
+            super().save_model(request, obj, form, change)
+        else:
+            booking = BookingService.create(
+                user=form.cleaned_data["user"],
+                seat_session=form.cleaned_data["seat_session"],
+            )
+
+            obj.pk = booking.pk
