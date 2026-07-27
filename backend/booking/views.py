50f6449e114df3e9905e460730bd3c145de6f026
artem <SS4T46@yandex.ru>
2026-07-27 14:11:50 +0300

    feat: update booking openAPI schema

--- backend/booking/views.py
+++ backend/booking/views.py
@@ -16,6 +16,10 @@ from .services import BookingService
 
 
 class BookingRetrieveView(APIView):
+    @extend_schema(
+        summary="Find a reservation instance by seat ID",
+        description="Return a single object",
+    )
     def get(self, request, seat_session_pk):
         booking_instance = BookingService.get_object(
             request=request, seat_session=seat_session_pk
@@ -27,7 +31,10 @@ class BookingRetrieveView(APIView):
 class BookingCreateView(APIView):
     @silk_profile()
     @extend_schema(
-        request=BookingWriteSerializer, responses={201: BookingReadSerializer}
+        request=BookingWriteSerializer,
+        responses={201: BookingReadSerializer},
+        summary="Reserve a seat in the hall",
+        description='Reserve a seat in the hall. Assign the seat status to "draft." If the reservation is not paid for within 5 minutes, the seat status will be reset and the reservation will be deleted.',
     )
     def post(self, request):
         serializer = BookingWriteSerializer(data=request.data)
@@ -46,7 +53,7 @@ class BookingCreateView(APIView):
 
 class BookingDeleteView(APIView):
     # add permission
-
+    @extend_schema(summary="Delete a seat in the hall")
     def delete(self, request, pk: None):
         booking = get_object_or_404(Booking, pk=pk)
         BookingService.delete(booking=booking)
@@ -59,6 +66,7 @@ class BookingListView(APIView):
         parameters=[
             OpenApiParameter("venue_pk", type=int, description="search by venue")
         ],
+        summary="Search all reservations, you can also specify the venue",
     )
     def get(self, request):
         booking_list = Booking.objects.select_related(
@@ -78,6 +86,10 @@ class BookingListView(APIView):
 
 
 class BookingPayView(APIView):
+    @extend_schema(
+        summary="Pay reservation",
+        description='This is a FAKE payment. After receiving the response, insert the UUI from "payment_url" into the "/api/payment/webhook" payment_id. This will reserve a seat in the event session.',
+    )
     def post(self, request, pk: int | None):
         try:
             result = PaymentService.create_payment(user=request.user, booking_pk=pk)
