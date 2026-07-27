0ab70f9a3b2ed5b35c7b9b994a1d66f227319fbb
artem <SS4T46@yandex.ru>
2026-07-27 13:20:50 +0300

    feat: add django-silk profiler

--- backend/config/django/base.py
+++ backend/config/django/base.py
@@ -166,3 +166,4 @@ from config.settings.drf_standartized_errors import *
 from config.settings.rest_framework import *
 from config.settings.django_debug_toolbar import *
 from config.settings.celery_configuration_options import *
+from config.settings.django_silk import *
