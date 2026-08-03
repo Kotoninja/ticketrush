bae18db891da65dd5157e6bb6998f60929797c6c
artem <SS4T46@yandex.ru>
2026-08-03 17:35:54 +0300

    build: redis configuration for channel layer

--- backend/config/settings/channels.py
+++ backend/config/settings/channels.py
@@ -1,2 +1,12 @@
 # Daphne
 ASGI_APPLICATION = "config.asgi.application"
+
+# Channels
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": ["redis://redis:6379/0?socket_timeout=10"],
+        },
+    },
+}
